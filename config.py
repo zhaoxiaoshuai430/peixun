daff3fc027dbc94fd6f4dcca7c0fae79ebc3622f
zhaoxiaoshuai430 <2896984429@qq.com>
2025-08-11 14:09:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
+# config.py
+import os
+from dotenv import load_dotenv
 
+# 加载本地 .env 文件（仅开发环境）
+load_dotenv()
 
 def DB_CONFIG():
     return {
-        "host": "192.168.3.214",
-        "user": "root",
-        "password": "hzjd8888",
-        "database": "exercises"
+        "host": os.getenv("DB_HOST"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "database": os.getenv("DB_NAME")
     }
 
-
 def ADMIN_PASSWORD():
-    return "zhongbu123456"
+    return os.getenv("ADMIN_PASSWORD")
